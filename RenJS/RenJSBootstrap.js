df3be99ed5e1c071406303ee735c6365c1ccdb97
Lam Ngo <ngobasonlam@gmail.com>
2018-08-04 13:51:20 -0400

    tryingt to fix speed difference between machine

--- RenJS/RenJSBootstrap.js
+++ RenJS/RenJSBootstrap.js
@@ -11,6 +11,8 @@ var bootstrap = {
     game.scale.scaleMode = Phaser.ScaleManager[globalConfig.scaleMode];
     game.scale.setUserScale(2,2,0,0);
     game.scale.refresh();
+
+    game.time.advancedTiming = true;
   },
 
   preload: function () {
