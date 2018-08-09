b3e58634f4eaea5c0accf35859c54a03d4880a79
Lam Ngo <ngobasonlam@gmail.com>
2018-08-09 14:20:01 -0400

    fix position of text box

--- RenJS/Effects.js
+++ RenJS/Effects.js
@@ -73,7 +73,7 @@ RenJS.effects = {
         RenJS.gui.hud.messageBox.destroy();
         RenJS.gui.initHUD("real");
         RenJS.gui.hud.messageBox.x += 0;
-        RenJS.gui.hud.messageBox.y += 14;
+        RenJS.gui.hud.messageBox.y = 279;
         RenJS.gui.hud.text.x = 20;
         RenJS.gui.showHUD();
 
