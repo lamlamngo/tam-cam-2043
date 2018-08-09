b8d03479632126dd94d6ebbfdca4ca2be52a5532
Lam Ngo <ngobasonlam@gmail.com>
2018-08-09 12:28:00 -0400

    add scenes at the workshop with animation

--- RenJS/Defaults.js
+++ RenJS/Defaults.js
@@ -28,7 +28,7 @@ var defaults = {
         CENTER: {x:globalConfig.w/2,y:globalConfig.h/2 - 25},
         CENTERREAL: {x:globalConfig.w/2,y:globalConfig.h/2},
         RIGHT: {x:(globalConfig.w/6)*5,y:globalConfig.h/2 - 16},
-        OUTRIGHT: {x:(globalConfig.w/6)*7,y:globalConfig.h/2},
+        OUTRIGHT: {x:(globalConfig.w/6)*7,y:globalConfig.h/2 - 16},
         START: {x: 1000, y:globalConfig.h/2 - 25},
         END: {x: 300, y: 300}
     },
